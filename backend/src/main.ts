6ac888e8d2d560ddfce37c0eed046878823c6226
Abhishek-Ag-1112 <your_email@example.com>
2026-07-17 12:12:37 +0530

    optimisted website

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -65,7 +65,7 @@ async function bootstrap() {
 
   // 4. Server Start
   const port = process.env.PORT || 4000;
-  await app.listen(port);
+  await app.listen(port, "0.0.0.0");
   console.log(`🚀 Element 5 Core Backend is running on: http://localhost:${port}`);
   console.log(`📖 API Documentation is available at: http://localhost:${port}/api/docs`);
 }
